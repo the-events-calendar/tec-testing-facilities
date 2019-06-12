a777a33e7f75ed5b88c818a9e84f1a5f89ae631b
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-06-12 14:55:34 +0200

    test(ItWorksTest.php): remove unused methods

--- tests/wpunit/ItWorksTest.php
+++ tests/wpunit/ItWorksTest.php
@@ -2,23 +2,6 @@
 
 class ItWorksTest extends \Codeception\TestCase\WPTestCase
 {
-
-    public function setUp(): void
-    {
-        // Before...
-        parent::setUp();
-
-        // Your set up methods here.
-    }
-
-    public function tearDown(): void
-    {
-        // Your tear down methods here.
-
-        // Then...
-        parent::tearDown();
-    }
-
     // Tests
     public function test_it_works()
     {
